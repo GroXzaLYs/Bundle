aa023209baba49fb3297f05b283e65e875328b92
GroXzaLYs <144025939+GroXzaLYs@users.noreply.github.com>
2026-01-04 07:06:25 +0700

    Implement ItemFactory class for item registry

---
+++ src/KnosTx/Bundle/item/ItemFactory.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace KnosTx\Bundle\item;
+
+use Closure;
+use KnosTx\Bundle\registry\ObjectRegistry;
+use pocketmine\item\Item;
+use pocketmine\item\ItemTypeIds;
+use pocketmine\item\ItemIdentifier as IID;
+
+final class ItemFactory extends ObjectRegistry{
+
+	protected static function registerDefaults() : void{
+		
+	}
+
+	public static function register(string $name, Closure $createItem) : Item{
+		$reflect = new \ReflectionClass(ItemTypeIds::class);
+		$typeId = $reflect->getConstant(mb_strtoupper($name));
+
+		if(!is_int($typeId)){
+			\GlobalLogger::get()->warning(
+				self::class . ": No ItemTypeId for $name, generating new one"
+			);
+			$typeId = ItemTypeIds::newId();
+		}
+
+		$item = $createItem(new IID($typeId));
+
+		self::registerObject($name, $item);
+
+		return $item;
+	}
+}
