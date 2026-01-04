bd9d4a4ee732707e3fef11f5d0487ebf8da73ab2
GroXzaLYs <144025939+GroXzaLYs@users.noreply.github.com>
2026-01-04 07:21:21 +0700

    Refactor Bundle class for improved item management

--- src/KnosTx/Bundle/item/Bundle.php
+++ src/KnosTx/Bundle/item/Bundle.php
@@ -5,60 +5,73 @@ declare(strict_types=1);
 namespace KnosTx\Bundle\item;
 
 use pocketmine\item\Item;
-use pocketmine\item\VanillaItems;
+use pocketmine\item\ItemIdentifier;
 use pocketmine\inventory\InventoryHolder;
 use pocketmine\inventory\SimpleInventory;
 use pocketmine\nbt\tag\CompoundTag;
 use pocketmine\nbt\tag\ListTag;
 
-class Bundle extends Item implements InventoryHolder{
-    private const MAX_CAPACITY = 64;
-    private SimpleInventory $inventory;
-    public const BUNDLE = 88888;
+final class Bundle extends Item implements InventoryHolder{
 
-    public function __construct(){
-        parent::__construct(self::BUNDLE, "Bundle");
-        $this->inventory = new SimpleInventory(1);
-    }
+	private const MAX_CAPACITY = 64;
 
-    public function getInventory(): SimpleInventory{
-        return $this->inventory;
-    }
+	private SimpleInventory $inventory;
 
-    public function addItem(Item $item): bool{
-        $totalWeight = 0;
-        foreach($this->inventory->getContents() as $content){
-            $totalWeight += $this->getItemSize($content);
-        }
-        $totalWeight += $this->getItemSize($item);
+	public function __construct(ItemIdentifier $identifier){
+		parent::__construct($identifier, "Bundle");
+		$this->inventory = new SimpleInventory(1);
+	}
 
-        if($totalWeight > self::MAX_CAPACITY){
-            return false;
-        }
+	public function getMaxStackSize() : int{
+		return 1;
+	}
 
-        $this->inventory->addItem($item);
-        return true;
-    }
+	public function getInventory() : SimpleInventory{
+		return $this->inventory;
+	}
 
-    private function getItemSize(Item $item): int{
-        return $item->getMaxStackSize() === 1 ? self::MAX_CAPACITY : (int) ceil(self::MAX_CAPACITY / $item->getMaxStackSize());
-    }
+	public function addItem(Item $item) : bool{
+		if($item instanceof self){
+			return false;
+		}
 
-    public function onSaveNBT(): CompoundTag{
-        $nbt = new CompoundTag();
-        $items = [];
-        foreach ($this->inventory->getContents() as $item) {
-            $items[] = $item->nbtSerialize();
-        }
-        $nbt->setTag("BundleItems", new ListTag($items));
-        return $nbt;
-    }
+		$used = 0;
+		foreach($this->inventory->getContents() as $content){
+			$used += $this->getWeight($content);
+		}
 
-    public function onLoadNBT(CompoundTag $nbt): void{
-        if ($nbt->getTag("BundleItems") !== null) {
-            foreach ($nbt->getListTag("BundleItems")->getValue() as $itemTag) {
-                $this->inventory->addItem(Item::nbtDeserialize($itemTag));
-            }
-        }
-    }
+		if($used + $this->getWeight($item) > self::MAX_CAPACITY){
+			return false;
+		}
+
+		$this->inventory->addItem($item);
+		return true;
+	}
+
+	private function getWeight(Item $item) : int{
+		return $item->getMaxStackSize() === 1
+			? self::MAX_CAPACITY
+			: (int) ceil(self::MAX_CAPACITY / $item->getMaxStackSize());
+	}
+
+	protected function serializeCompoundTag(CompoundTag $nbt) : void{
+		$list = [];
+
+		foreach($this->inventory->getContents() as $item){
+			$list[] = $item->nbtSerialize();
+		}
+
+		$nbt->setTag("BundleItems", new ListTag($list));
+	}
+
+	protected function deserializeCompoundTag(CompoundTag $nbt) : void{
+		$list = $nbt->getListTag("BundleItems");
+		if($list === null){
+			return;
+		}
+
+		foreach($list as $tag){
+			$this->inventory->addItem(Item::nbtDeserialize($tag));
+		}
+	}
 }
